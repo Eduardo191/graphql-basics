390840f88791d8890ca9f2fa65f4ee1334dc9844
unknown <feduardo191@gmail.com>
2020-05-19 15:26:31 -0300

    Trabalhando com arrays

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+//Demo users data
+const users = [{
+  id: '1',
+  name: 'Eduardo',
+  email: 'edu@example.com',
+  age: 19
+}, {
+  id: '2',
+  name: 'Sarah',
+  email: 'sarah@example.com',
+  age: 23
+}, {
+  id: '3',
+  name: 'James',
+  email: 'james@example.com',
+  age: 56
+}]
+
+const posts = [{
+  id: '1',
+  title: 'First post',
+  body: 'Pay the water bill',
+  published: true
+}, {
+  id: '2',
+  title: 'Second post',
+  body: 'College tasks',
+  published: false
+}, {
+  id: '3',
+  title: 'Third post',
+  body: 'Clear the house',
+  published: true
+}]
+
 //Type definitions (schema)
 const typeDefs = `
   type Query {
-    add(a: Float!, b: Float!): Float!
-    greeting(name: String): String!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!
     post: Post!
   }
@@ -27,15 +62,25 @@ const typeDefs = `
 //Resolvers
 const resolvers = {
   Query: {
-    add(parent, args, ctx, info) {
-      return args.a + args.b
-    },
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello ${args.name}!`
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
       }
 
-      return 'Hello'
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+        const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+        return isTitleMatch || isBodyMatch  
+      })
     },
     me() {
       return {
