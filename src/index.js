a80ed93e9c0c913bcebe2a534071ac8f87207fa8
unknown <feduardo191@gmail.com>
2020-05-20 16:31:19 -0300

    Adicioando relacionamento entre comments e posts

--- src/index.js
+++ src/index.js
@@ -21,35 +21,39 @@ const users = [{
 const comments = [{
   id: '1',
   text: 'Im learning right now',
-  author: '3'
+  author: '3',
+  post: '36'
 }, {
   id: '2',
   text: 'This is Graphql',
-  author: '2'
+  author: '2',
+  post: '22'
 }, {
   id: '3',
   text: 'Graphql is amazing',
-  author: '1'
+  author: '1',
+  post: '11'
 }, {
   id: '4',
   text: 'I love graphql',
-  author: '2'
+  author: '2',
+  post: '36'
 }]
 
 const posts = [{
-  id: '1',
+  id: '11',
   title: 'First post',
   body: 'Pay the water bill',
   published: true,
   author: '1'
 }, {
-  id: '2',
+  id: '22',
   title: 'Second post',
   body: 'College tasks',
   published: false,
-  author: '1'
+  author: '31'
 }, {
-  id: '3',
+  id: '36',
   title: 'Third post',
   body: 'Clear the house',
   published: true,
@@ -81,12 +85,14 @@ const typeDefs = `
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
   }
 
   type Comment {
     id: ID!
     text: String!
     author: User!
+    post: Post!
   }
 `
 
@@ -138,12 +144,22 @@ const resolvers = {
       return users.find((user) => {
         return user.id === parent.author
       })
+    }, 
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id
+      })
     }
   },
   Comment: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
         return user.id === parent.author
+      })
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post
       })
     }
   },
